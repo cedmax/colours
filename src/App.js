68c6f15fcc7123c4df1a75a9215bbc0ca472ff8c
Marco Cedaro <cedmax@users.noreply.github.com>
2019-02-03 11:03:14 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
       .toHexString();
 
     this.setState({
-      background: color.hex,
+      background: color, 
       color: readAble,
     });
   };
