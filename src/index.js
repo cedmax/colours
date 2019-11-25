c1d7f0be25fec2be29920a435c1e2d7c46cf40d8
Marco Cedaro <cedmax@users.noreply.github.com>
2019-11-25 16:27:56 +0000

    rollback to original app file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import App from "./App";
+import App from "./App";
 // import App from "./cheating/AppFunc";
 // import App from "./cheating/AppFuncWithHistory";
-import App from "./cheating/AppFuncWithHistoryHook";
+// import App from "./cheating/AppFuncWithHistoryHook";
 // import App from "./cheating/AppFuncWithHistoryHookAndReducers";
 // import App from "./cheating/AppFuncWithHistoryHookAndReducersSTATE";
 import "./style.css";
