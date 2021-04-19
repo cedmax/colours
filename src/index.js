4c43c27b98b88e16f5353e25b0d00ce21d0660a3
Marco Cedaro <cedmax@users.noreply.github.com>
2021-04-19 19:24:34 +0100

    service worker?

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from "./App";
 import "./style.css";
 
 const data = JSON.parse(document.getElementById("data").innerText);
 
 ReactDOM.render(<App data={data} />, document.getElementById("root"));
-serviceWorker.register();
+serviceWorkerRegistration.register();
